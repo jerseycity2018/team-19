a5ff9db224456198814e32998f867580a9cb5533
gillarious <isabella.berryx3@gmail.com>
2018-10-26 21:31:34 -0400

    edited admin page

---
+++ src/pages/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Admin extends Component {
+  render() {
+    return (
+      <div className="App">
+        hey
+      </div>
+    );
+  }
+}
+
+export default Admin;
