c99ed867ce9acebf1833e9430cf12b41f6a59713
Pat Song <patsong@Pats-MacBook-Pro.local>
2018-10-26 22:03:22 -0400

    new branch for pat

---
+++ login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+
+
+
+    <label>First Name </label>,
+    <input type="text" name="firstName" required/>,<br/>,
+    <label>Last Name </label>,
+    <input type="text" name="lastName" required/>,<br/>,
+    <label>Username </label>,
+    <input type="text" name="username" required/>,<br/>,
+    <label>Password </label>,
+    <input type="text" name="password" required/>,<br/>,
+
+    document.getElementById('root')
+  );
+  
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -5,8 +5,17 @@ class LogIn extends Component {
   render() {
     return (
       <div className="App">
-        hey
-      </div>
+
+    <label>Username </label>
+    <input type="text" name="username" required/><br/>
+    <label>Password </label>
+    <input type="text" name="password" required/><br/>
+
+    <button>Log In </button>
+    <button>Sign Up </button>
+
+
+    </div>
     );
   }
 }
