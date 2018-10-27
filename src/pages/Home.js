b53a5d5897328e232d00b6076ebf88487079c4ed
Mike Scuderi <mikescuderi@Mikes-MacBook-Air.local>
2018-10-27 02:44:42 -0400

    scoreboard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Tabs, Tab} from 'react-bootstrap-tabs';
-import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap'
+import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, Table } from 'reactstrap'
 import classnames from 'classnames';
 import '../App.css';
 
@@ -24,7 +24,7 @@ class Home extends Component {constructor(props) {
     return (
       <div>
         <Container className="Greetings">
-            <h1> Hello!</h1>
+            <h1> Hello, Michael!</h1>
         </Container>
         <Nav tabs>
             <NavItem>
@@ -40,7 +40,7 @@ class Home extends Component {constructor(props) {
                 className={classnames({ active: this.state.activeTab === '2' })}
                 onClick={() => { this.toggle('2'); }}
                 >
-                Scoreboard
+                Monthly Scoreboard
                 </NavLink>
             </NavItem>
             <NavItem>
@@ -64,24 +64,92 @@ class Home extends Component {constructor(props) {
                 <Row>
                     <Col sm="6">
                         <Card body>
-                            <CardTitle>Special Title Treatment</CardTitle>
-                            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                            <Button>Go somewhere</Button>
+                            <CardTitle id = "App">&nbsp; Borough Rankings</CardTitle>
+                            <Table>
+                                <thead>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Borough</th>
+                                    <th>Work (hr)</th>
+                                    <th>Compost (lb)</th>
+                                    <th>Score</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Brooklyn</td>
+                                    <td>27</td>
+                                    <td>217</td>
+                                    <td>234</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Bronx</td>
+                                    <td>22</td>
+                                    <td>180</td>
+                                    <td>202</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Staten Island</td>
+                                    <td>18</td>
+                                    <td>199</td>
+                                    <td>217</td>
+                                </tr>
+                                </tbody>
+                            </Table>
                         </Card>
                     </Col>
                     <Col sm="6">
                         <Card body>
-                            <CardTitle>Special Title Treatment</CardTitle>
-                            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                            <Button>Go somewhere</Button>
+                        <CardTitle id = "App">&nbsp; Individual Rankings</CardTitle>
+                        <Table>
+                                <thead>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Name</th>
+                                    <th>Work (hr)</th>
+                                    <th>Compost (lb)</th>
+                                    <th>Score</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Gerald Miller</td>
+                                    <td>7</td>
+                                    <td>32</td>
+                                    <td>39</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Katy Morgan</td>
+                                    <td>5</td>
+                                    <td>29</td>
+                                    <td>34</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Chase Hershey</td>
+                                    <td>2</td>
+                                    <td>31</td>
+                                    <td>33</td>
+                                </tr>
+                                </tbody>
+                            </Table>
                         </Card>
                     </Col>
                 </Row>
             </TabPane>
             <TabPane tabId="3">
                 <Row>
+                    
                     <Col sm="12">
-                        <h4>Tab 3 Contents</h4>
+                        <h4>Claim a reward!</h4>
+                    </Col>
+                    <Col>
+                        <button>Submit reward request</button>
                     </Col>
                 </Row>
             </TabPane>
