8429aa0f43bfc8854bb5494b21d1cd31ff69db2e
gillarious <isabella.berryx3@gmail.com>
2018-10-26 23:06:49 -0400

    Creating containers within admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap'
+import { Link } from 'react-router-dom'
 import '../App.css';
 
 class Admin extends Component {
   render() {
     return (
-      <Container>
-        hey
+      <Container className="Admin">
+        <Container>
+          hello
+          <Link to="/home">Home</Link>
+        </Container>
+        <Container>
+          lmao
+        </Container>
       </Container>
     );
   }
