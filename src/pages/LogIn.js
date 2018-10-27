37ab052b59d32c538a66e136f054434148d4a3c0
gillarious <isabella.berryx3@gmail.com>
2018-10-26 21:28:15 -0400

    edited log in

---
+++ src/pages/LogIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LogIn extends Component {
+  render() {
+    return (
+      <div className="App">
+        hey
+      </div>
+    );
+  }
+}
+
+export default LogIn;
