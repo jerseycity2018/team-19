5d243403861ba85308967c17571bcb84469c268b
at870 <austintsai7@gmail.com>
2018-10-27 03:57:34 -0400

    Beginning of Rewards Page

---
+++ src/pages/Rewards.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Rewards extends Component {
+  render() {
+    return (
+      <div className="App">
+        Heyo
+      </div>
+    );
+  }
+}
+
+export default Rewards;
