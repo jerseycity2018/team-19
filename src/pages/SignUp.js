e4b0bb0e070c0e2ea42f936d28c77f285c330568
gillarious <isabella.berryx3@gmail.com>
2018-10-26 21:33:12 -0400

    added css to pages

---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="App">
+        hey
+      </div>
+    );
+  }
+}
+
+export default SignUp;
